b7a8102fa664b2f9687f77f9f234513a2e6c6ff3
ZiskoDiaz <86810034+ZiskoDiaz@users.noreply.github.com>
2021-07-02 02:19:54 -0400

    Create operation_request_user.py
    An script to resolve a arithmetic operation with the request to 2 number and resolve

---
+++ operation_request_user.py
@@ -0,0 +1,9 @@
+n = int(input("ingresa un numero entero cualquiera: "))
+m = int(input("ingresa un numero entero cualquiera: "))
+# 7.a.- n*m
+entre = n*m
+# 7.b.- n//m
+cociente = n//m
+# 7.c.- (n%m)
+resto = n%m
+print("el resultado de ",n ,"entre ", m ,"es ", entre, " el cociente es ", cociente, "y el resto es de ", resto)
