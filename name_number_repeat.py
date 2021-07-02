39a5b8546e8ed7e3e3776f6663dd5d3c0ddf8a3f
ZiskoDiaz <86810034+ZiskoDiaz@users.noreply.github.com>
2021-07-02 00:29:34 -0400

    Create name_number_repeat.py
    introduce name and number, and after, the name repeat again end number.

---
+++ name_number_repeat.py
@@ -0,0 +1,6 @@
+nombre_persona = input("ingresa tu nombre: ")
+numero = int(input("ingresa un numero entero cualquiera: "))
+contador = 0
+while contador<numero:
+    print (nombre_persona)
+    contador += 1
