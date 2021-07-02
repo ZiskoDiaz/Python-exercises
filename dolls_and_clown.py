04c685097c7232abd9309fd05dfeffb12af85f32
ZiskoDiaz <86810034+ZiskoDiaz@users.noreply.github.com>
2021-07-02 02:33:18 -0400

    Create dolls_and_clown.py
    cualculate kilogram of package

---
+++ dolls_and_clown.py
@@ -0,0 +1,8 @@
+payaso = int(input("Ingrese cuantos payasos va a comprar: "))
+muneca = int(input("ingresa cuantas munecas va a comprar: "))
+
+peso_payasos = payaso*112
+peso_munecas = muneca*75
+peso_final_paquete = peso_payasos+peso_munecas
+
+print (" En total Usted lleva ", payaso, " payasos y lleva ", muneca, " el paquete total pesa ",peso_final_paquete," gramos")
