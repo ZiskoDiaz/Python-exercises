ff491fdbcaabf0c7840238766fe1bcdbdc2bd338
ZiskoDiaz <86810034+ZiskoDiaz@users.noreply.github.com>
2021-07-02 02:36:51 -0400

    Create assess_age.py
    assess age script

---
+++ assess_age.py
@@ -0,0 +1,12 @@
+edad = int(input("ingrese su edad " ))
+if edad < 4:
+    print ("entra gratis")
+
+if edad >= 4 and edad <=18:
+    print ("Paga 500")
+
+if edad > 18:
+    print ("Tu pagas 1000")
+
+if edad < 1 :
+    print ("Data Error")
