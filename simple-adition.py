298ac8e7ea8cf23e2a7656a9a3cc3d5a67909707
ZiskoDiaz <86810034+ZiskoDiaz@users.noreply.github.com>
2021-07-10 16:53:40 -0400

    Create simple-adition.py
    first play on python

---
+++ simple-adition.py
@@ -0,0 +1,6 @@
+# calculadora  solo suma
+primero = int(input('ingresa el primer numero: '))
+segundo = int(input('ingresa el segundo numero: '))
+calcular = primero + segundo
+
+print("El total es" , calcular)
