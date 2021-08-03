fdad5d4e734b26d275d796e4876c62304f6cdd30
ZiskoDiaz <86810034+ZiskoDiaz@users.noreply.github.com>
2021-08-03 01:11:51 -0400

    Create playing_with_objects.py
    how to delete an object and instance in py

---
+++ playing_with_objects.py
@@ -0,0 +1,24 @@
+# creador de clases
+class Usuario:
+    # metodos
+    def __init__(self, nombre, apellido, edad):
+        # instanciamos
+        self.nombre = nombre
+        self.apellido = apellido
+        self.edad = edad
+
+    def saludo(self):
+        print("Hola, mi nombre es " , self.nombre, self.apellido, "y mi edad es " , self.edad)
+
+# generamos una instancia
+usuario = Usuario("Francisco", "Diaz", 29) #llamamos a la clase de usuario
+
+usuario.saludo()
+usuario.nombre = "Juan"
+# eliminar instancia
+del usuario.nombre
+usuario.saludo()
+
+# eliminar objeto
+del usuario
+print(usuario)
